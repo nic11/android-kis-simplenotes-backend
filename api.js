49171ba3dfc6e5ee225b4060b367cf3ff4de1fd0
Roman Nikonov <niconneo@yandex.ru>
2020-05-23 07:08:20 +0300

    register api endpoint; working with images (resizing and stuff)

--- api.js
+++ api.js
@@ -5,8 +5,9 @@ const express = require('express');
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
 const {addAsync} = require('@awaitjs/express');
+const sharp = require('sharp');
 
-const {Note} = require('./db');
+const {Note, User} = require('./db');
 const secrets = require('./secrets');
 
 const auth = passport.authenticate('jwt', {session: false});
@@ -41,18 +42,69 @@ router.post('/login', (req, res, next) => {
   })(req, res, next);
 });
 
+router.post('/register', (req, res, next) => {
+  User.register(new User({username: req.body.username}), req.body.password, (err, account) => {
+    if (err) {
+      return next(err);
+    }
+    console.log('registered ', account);
+    passport.authenticate('login', (err, user, info) => {
+      if (err) {
+        return next(err);
+      }
+      if (!user) {
+        // likely auth error
+        return next(new Error('user is falsy'));
+      }
+      req.login(user, {session: false}, (err) => {
+        if (err) {
+          return next(err);
+        }
+        const payload = {user: {_id: user._id, username: user.username}};
+        jwt.sign(payload, secrets.JWT_KEY, (err, token) => {
+          if (err) {
+            return next(err);
+          }
+          return res.json({token});
+        });
+      });
+    })(req, res, next);
+  });
+});
+
 router.get('/me', auth, (req, res) => {
   res.json(req.user);
 });
 
 router.getAsync('/notes', auth, async (req, res) => {
   const docs = await Note.find({userId: req.user._id}).exec();
-  res.json({ok: true, notes: docs});
+  const notes = docs.map((doc) => ({
+    _id: doc._id,
+    text: doc.text,
+    pictureB64: doc.picture? doc.picture.toString('base64') : null,
+    archived: doc.archived,
+    createdAt: doc.createdAt,
+    updatedAt: doc.updatedAt,
+  }));
+  res.json({ok: true, notes});
   // debug('kek')
 });
 
 router.postAsync('/notes', auth, async (req, res) => {
-  const note = new Note({...req.body, userId: req.user._id});
+  const picture =
+    req.body.imageB64?
+      await sharp(Buffer.from(req.body.imageB64, 'base64'))
+        .rotate()  // fix EXIF rotation tag
+        .resize({width: 480, height: 640, fit: 'cover'})
+        .toBuffer()
+    : null;
+  const noteRaw = {
+    text: req.body.text,
+    picture,
+    archived: req.body.archived,
+    userId: req.user._id,
+  };
+  const note = new Note(noteRaw);
   console.log(note);
   await note.save();
   res.json({ok: true, noteId: note._id});
@@ -60,7 +112,6 @@ router.postAsync('/notes', auth, async (req, res) => {
 
 router.putAsync('/notes/:id', auth, async (req, res) => {
   const note = await Note.findById(req.params.id).exec();
-  console.log(note);
   if (!note || note.userId.toString() != req.user._id) {
     return res.status(404).json({ok: false, message: 'Note not found or no access'});
   }
@@ -68,6 +119,7 @@ router.putAsync('/notes/:id', auth, async (req, res) => {
     note[k] = v;
   }
   await note.save();
+  console.log(note);
   res.json({ok: true, noteId: note._id});
 });
 
